286c801cb65f39c12fbb1408437025d4e44393e7
SourabhSisodia <sourabhsisodia010@gmail.com>
2022-03-11 23:16:00 +0530

    added loader

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 let apiQuotes = [];
-const quoteContainer  = document.getElementById("quote");
+const quoteContainer = document.getElementById("quote-container");
+const quoteText  = document.getElementById("quote");
 const author  = document.getElementById("author");
 const tweetButton = document.getElementById("twitter");
 const newQuoteButton  = document.getElementById("new-quote");
-
+const loader = document.getElementById("loader");
 // generate new quote
 const newQuote = ()=> {
+    loading();
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    quoteContainer.innerText = quote.text;
+    quoteText.innerText = quote.text;
     author.innerText = quote.author;
-    if(quoteContainer.textContent.length > 100)
-        quoteContainer.classList.add('long-text');
+    if(quoteText.textContent.length > 100)
+        quoteText.classList.add('long-text');
     else
-        quoteContainer.classList.remove('long-text');
+        quoteText.classList.remove('long-text');
+    complete();
 }
 // api calling
 async function getQuotes(){
+    loading();
     const apiUrl = "https://type.fit/api/quotes";
     try{
         const response = await fetch(apiUrl);
@@ -29,8 +33,18 @@ async function getQuotes(){
 }
 // tweet button
 function tweetQuote(){
-    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteContainer.textContent} ~ ${author.textContent}`;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} ~ ${author.textContent}`;
     window.open(twitterUrl,'_blank');
+}
+// loading 
+function loading(){
+    loader.hidden = false;
+    quoteContainer.hidden =true;    
+}
+// hide loading
+function complete(){
+    quoteContainer.hidden =false;
+    loader.hidden =true;
 }
 newQuoteButton.onclick = newQuote;
 tweetButton.onclick = tweetQuote;
